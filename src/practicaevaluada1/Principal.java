3134627f5a109699639e24f48a37deae8695b36f
MaoMore24 <mao.ms27@gmail.com>
2025-10-04 18:14:39 -0600

    Hola Mundo

--- src/practicaevaluada1/Principal.java
+++ src/practicaevaluada1/Principal.java
@@ -8,7 +8,7 @@ package practicaevaluada1;
 public class Principal {
 
     public static void main(String[] args) {
-        
+        System.out.println("Hola Mundo");
                 
                 
     }
