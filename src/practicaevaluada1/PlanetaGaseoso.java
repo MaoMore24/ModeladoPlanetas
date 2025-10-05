528279cbb8ecf30371a95f51a3caa82d5e06c9e0
MaoMore24 <mao.ms27@gmail.com>
2025-10-04 19:07:21 -0600

    Se agrega clase rocoso y gaseoso y planeta

--- src/practicaevaluada1/PlanetaGaseoso.java
+++ src/practicaevaluada1/PlanetaGaseoso.java
@@ -2,6 +2,62 @@
 package practicaevaluada1;
 
 
-public class PlanetaGaseoso {
+public class PlanetaGaseoso extends Planet {
+        
+    private String composicion, tipoSuperficie, caractTamaño, tipoNucleo;
+
+    public PlanetaGaseoso(String composicion, String tipoSuperficie, String caractTamaño, String tipoNucleo, double tamaño, double distanciaSol, int numLunas) {
+        super(tamaño, distanciaSol, numLunas);
+        this.composicion = composicion;
+        this.tipoSuperficie = tipoSuperficie;
+        this.caractTamaño = caractTamaño;
+        this.tipoNucleo = tipoNucleo;
+    }
+
+    public String getComposicion() {
+        return composicion;
+    }
+
+    public void setComposicion(String composicion) {
+        this.composicion = composicion;
+    }
+
+    public String getTipoSuperficie() {
+        return tipoSuperficie;
+    }
+
+    public void setTipoSuperficie(String tipoSuperficie) {
+        this.tipoSuperficie = tipoSuperficie;
+    }
+
+    public String getCaractTamaño() {
+        return caractTamaño;
+    }
+
+    public void setCaractTamaño(String caractTamaño) {
+        this.caractTamaño = caractTamaño;
+    }
+
+    public String getTipoNucleo() {
+        return tipoNucleo;
+    }
+
+    public void setTipoNucleo(String tipoNucleo) {
+        this.tipoNucleo = tipoNucleo;
+    }
+
+        
+
+    @Override
+    public String toString() {
+        return super.toString() + "Planeta Gaseoso: " + "\nComposición = " + composicion + 
+                "\nTipo de Superficie = " + tipoSuperficie + "Tamaño = " + caractTamaño + 
+                "\nTipo de Núcleo=" + tipoNucleo + ".";
+    }
     
+    
+    
+    
+    
+     
 }
