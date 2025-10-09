0f1b58df9fcc6d2565d7454b5d1796dc5cc0c52f
MaoMore24 <mao.ms27@gmail.com>
2025-10-08 22:23:54 -0600

    Interface

--- src/practicaevaluada1/Planeta.java
+++ src/practicaevaluada1/Planeta.java
@@ -3,5 +3,10 @@ package practicaevaluada1;
 
 
 public interface Planeta {
-    
+    String getNombre();
+    double getTamaño();
+    double getDistanciaAlSol();
+    int getNumeroLunas();
+    double calcularAño(); //Tiempo que tarda en dar la vuelta al sol en días
+    String getTipo(); //Gaseoso o Rocoso
 }
