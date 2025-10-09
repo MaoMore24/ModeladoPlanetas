632231129f5d8296d2538e80961030d387489734
MaoMore24 <mao.ms27@gmail.com>
2025-10-08 19:53:54 -0600

    Inclusión de clase abstracta e interfaz

--- src/practicaevaluada1/CuerpoCeleste.java
+++ src/practicaevaluada1/CuerpoCeleste.java
@@ -2,6 +2,62 @@
 package practicaevaluada1;
 
 
-public class CuerpoCeleste {
+public abstract class CuerpoCeleste implements Planeta {
+     private String nombre;
+    private double tamaño; //km
+    private double distanciaAlSol; //millones de Km
+    private int numeroLunas; 
+
+    public CuerpoCeleste(String nombre, double tamaño, double distanciaAlSol, int numeroLunas) {
+        this.nombre = nombre;
+        this.tamaño = tamaño;
+        this.distanciaAlSol = distanciaAlSol;
+        this.numeroLunas = numeroLunas;
+    }
     
+   
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public double getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(double tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    @Override
+    public double getDistanciaAlSol() {
+        return distanciaAlSol;
+    }
+
+    public void setDistanciaAlSol(double distanciaAlSol) {
+        this.distanciaAlSol = distanciaAlSol;
+    }
+
+    @Override
+    public int getNumeroLunas() {
+        return numeroLunas;
+    }
+
+    public void setNumeroLunas(int numeroLunas) {
+        this.numeroLunas = numeroLunas;
+    }
+
+    @Override
+    public abstract double calcularAño();
+    
+    @Override
+    public abstract String getTipo();
+
+       
 }
